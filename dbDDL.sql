7b90fe6e58c588c84512013367f3437770bfeab4
Aser <techdr95@gmail.com>
2021-04-09 17:44:44 -0400

    Added instructions for next phase, phase IV steps 10, 11, 12, 13

---
+++ dbDDL.sql
@@ -0,0 +1,9 @@
+-- Make a file containing the SQL statements that 
+-- create your entire database schema, named dbDDL.sql. 
+-- This includes the tables with their constraints, 
+-- view, indexes, triggers, and all other database 
+-- objects if you have them.
+-- To keep the project consistent, make sure you 
+-- have at least 8 tables. Make sure you have at 
+-- least 2 database objects among trigger, function, 
+-- procedure or view.
---
+++ dbSQL.sql
@@ -0,0 +1,15 @@
+-- Create a script with queries from the relation algebra 
+-- in Step 7, named dbSQL.sql. This script
+-- should contain at least 5 queries on your database. 
+-- Use the comment facility in SQL*Plus 
+-- (starting a line with -- , or /* */ ) 
+-- to write the English version of your query, followed by the SQL version of the query. 
+-- Also show the expected output in the file. These queries need to satisfy the following:
+-- • Should be at least join queries (some involving more than 2 relations)
+-- • At least two of them should be aggregate queries including GROUP BY and
+-- HAVING clauses with ORDER BY clause as well
+-- • At least one of them should have nested subquery
+-- The purpose of having you write these is to make you 
+-- think about slightly complex scenarios on your database 
+-- schema and have you write queries involving join, aggregation 
+-- and nesting that you have learned in the class.
