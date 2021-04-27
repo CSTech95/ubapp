5cd7b76ed8eeca129fa77c077cd40f56fcba7411
Aser <techdr95@gmail.com>
2021-04-27 00:25:05 -0400

    created a sample query to work with

--- index.js
+++ index.js
@@ -3,10 +3,39 @@
 // https://cloud.google.com/appengine/docs/standard/nodejs/building-app/writing-web-service
 
 const express = require('express');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+// Create connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// Connect
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySql Connected...');
+});
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
+});
+
+app.get(`/addcourse1`, (req, res) => {
+  let course = { courseId: 1, courseName: `Art 101`, subject: `Art` };
+  let sql = `INSERT INTO COURSE SET ?`;
+  let query = db.query(sql, course, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(`course added…`);
+  });
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
