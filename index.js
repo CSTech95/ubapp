38c204e70f6e9126bd7ea1f1991ba874fafb64d2
Aser <techdr95@gmail.com>
2021-04-28 01:08:23 -0400

    extracted away db config to its own file

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
-require('dotenv').config();
-
-// Create connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+const db = require('./db');
 
 // Connect
 db.connect((err) => {
@@ -29,8 +21,10 @@ app.get('/', (req, res) => {
 });
 
 // Routes for courses
+
+// Add a course
 app.get(`/addcourse`, (req, res) => {
-  let course = { courseId: 5, courseName: `Art 105`, subject: `Art` };
+  let course = { courseName: `Algebra 2`, subject: `Math` };
   let sql = `INSERT INTO COURSE SET ?`;
   let query = db.query(sql, course, (err, result) => {
     if (err) throw err;
@@ -38,6 +32,37 @@ app.get(`/addcourse`, (req, res) => {
     res.send(`course added…`);
   });
 });
+
+// Get courses
+app.get(`/getcourses`, (req, res) => {
+  let sql = `SELECT * FROM COURSE`;
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.send(`Got courses`);
+  });
+});
+
+// Get a course
+app.get(`/getcourse/:id`, (req, res) => {
+  let sql = `SELECT * FROM COURSE WHERE courseId = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(`Got course`);
+  });
+});
+
+// // Update a course 23:00
+// app.get(`/updatecourse/:id`, (req, res) => {
+//   let sql = `SELECT * FROM COURSE WHERE courseId = ${req.params.id}`;
+//   let query = db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send(`Got course`);
+//   });
+// });
+
 // End of routes for courses
 
 // Listen to the App Engine-specified port, or 8080 otherwise
